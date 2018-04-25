9e0a0f92209f8592b5f2aa2467d015b5cfaf8c4c
buffer0x7cd <manvendra021997@gmail.com>
2018-04-25 18:11:20 +0530

    change index to 1 based in script

--- mac_generator.py
+++ mac_generator.py
@@ -5,14 +5,14 @@ import random
 def randomMAC():
     #set the locally administered and clear the multicast bit
 	mac = [ 0x02, 0x00, 0x00,
-		random.randint(0x00, 0x7f),
+		random.randint(0x00, 0xff),
 		random.randint(0x00, 0xff),
 		random.randint(0x00, 0xff) ]
 	return ':'.join(map(lambda x: "%02x" % x, mac))
 
 def main(n):
     print("id, ethernet address, wireless address")
-    for i in range(n):
+    for i in range(1,n+1):
         ethernetMac = randomMAC()
         wirelessMac = randomMAC()
         while wirelessMac == ethernetMac:
